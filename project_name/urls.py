bcf01f92670fb99b55672c75c2224a5c89c40bc0
Roman Telezhynskyi <dismine@gmail.com>
2019-10-04 17:35:53 +0300

    AttributeError: module 'django.contrib.auth.views' has no attribute 'login' on django 2.1.1

--- project_name/urls.py
+++ project_name/urls.py
@@ -4,20 +4,17 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
 """
 # Django imports
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', '{{ project_name }}.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls', namespace='blog')),
+    # path('', '{{ project_name }}.views.home', name='home'),
+    # path('blog/', include('blog.urls', namespace='blog')),
 
     # provide the most basic login/logout functionality
-    url(r'^login/$', auth_views.login,
-        {'template_name': 'core/login.html'}, name='core_login'),
-    url(r'^logout/$', auth_views.logout, name='core_logout'),
+    path('accounts/', include('django.contrib.auth.urls')),
 
     # enable the admin interface
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
